e821d3459548cf741f24aa3d874f9aa97b7cee10
Bened1ct-7 <111297090+Bened1ct-7@users.noreply.github.com>
2022-09-06 22:14:22 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,187 @@
+const word = document.querySelector('.word');
+const form = document.forms['form'];
+const userInput = form.querySelector('#input');
+const checkBtn = document.querySelector('.check');
+const refreshBtn = document.querySelector('.refresh');
+const hscoreText = document.querySelector('.high-score');
+const scoreText = document.querySelector('.score');
+const hint = document.querySelector('.hint');
+const timeText = document.querySelector('.time');
+
+let score = 0,
+  currentTime = 30,
+  time = currentTime;
+let scoreArray = JSON.parse(localStorage.getItem('high-score'));
+let highScore = Math.max(...scoreArray);
+hscoreText.innerText = highScore;
+
+let country, mainWord, shuffled, userGuess, random, div;
+
+String.prototype.shuffle = function() {
+  let a = this.split(""),
+    n = a.length;
+
+  for (let i = n - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+  return a.join("");
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  checkWord(userInput.value.toLowerCase().trim());
+});
+
+refreshBtn.addEventListener('click', reshuffle)
+
+function initGame() {
+  checkScoreArray();
+  fetchWord();
+  timeCheck();
+}
+
+initGame();
+
+function checkScoreArray() {
+  scoreArray = JSON.parse(localStorage.getItem('high-score'));
+  if (!scoreArray) {
+    scoreArray = [0];
+    highScore = Math.max(...scoreArray);
+    hscoreText.textContent = highScore;
+  }
+}
+
+function fetchWord() {
+  random = Math.floor(Math.random() * countries.length);
+  country = countries[random];
+  mainWord = country.name.toLowerCase();
+  shuffled = mainWord.shuffle();
+  console.log(mainWord);
+  word.innerText = shuffled.toUpperCase();
+  hint.innerText = country.hint;
+  userInput.focus();
+}
+
+function reshuffle() {
+  shuffled = shuffled.shuffle();
+  word.innerText = shuffled.toUpperCase();
+  userInput.focus();
+}
+
+function timeCheck() {
+  let myInterval = setInterval(() => {
+    if (time > 0) {
+      time--;
+      timeText.innerText = time;
+    }
+    if (time < 6) {
+      timeText.classList.add('red');
+    } else {
+      timeText.classList.remove('red');
+    }
+    if (time < 1) {
+      gameOver();
+      clearInterval(myInterval);
+    };
+  }, 1000)
+}
+
+function gameOver() {
+  checkScoreArray();
+  scoreArray.push(score);
+  highScore = Math.max(...scoreArray);
+  hscoreText.innerText = highScore;
+  localStorage.setItem('high-score', JSON.stringify(scoreArray));
+  userInput.disabled = true;
+  checkBtn.disabled = true;
+  refreshBtn.disabled = true;
+  displayMessage(`GAME OVER! The Country was ${mainWord.toUpperCase()}. Do you wish to play again?`, 'show')
+}
+
+function checkWord(guess) {
+  if (userInput.value !== '') {
+    if (guess === mainWord) {
+      userInput.classList.remove('wrong');
+      score++;
+      userInput.value = '';
+      scoreText.innerText = score;
+      scoreText.style.color = 'green';
+      setTimeout(() => scoreText.style.color = `#111`, 2000)
+      displayMessage(`Correct! The country was indeed ${mainWord.toUpperCase()}. Next Round!`)
+      time = currentTime + 3;
+      setTimeout(() => {
+        div.parentNode.removeChild(div);
+        fetchWord();
+      }, 2000)
+
+    } else {
+      userInput.classList.add('wrong');
+      displayMessage('Oops! Try again', '')
+      setTimeout(() => {
+        div.parentNode.removeChild(div)
+        userInput.focus();
+      }, 2000)
+    }
+  } else {
+    displayMessage('You need to enter a valid word!', '');
+    setTimeout(() => div.parentNode.removeChild(div), 3000)
+  }
+}
+
+
+
+function displayMessage(text, display) {
+  div = document.createElement('div');
+  div.className = 'msg-box';
+  document.body.appendChild(div);
+
+  const button = document.createElement('button');
+  button.innerText = 'X';
+  button.className = 'clear-btn';
+  div.appendChild(button);
+
+  const para = document.createElement('p');
+  para.className = 'msg-text';
+  para.innerText = text;
+  div.appendChild(para);
+
+  button.addEventListener('click', () => {
+    div.parentNode.removeChild(div);
+  })
+
+  if (display === 'show') {
+    const buttonDiv = document.createElement('div');
+    buttonDiv.className = 'button-div';
+    const yesBtn = document.createElement('button');
+    yesBtn.className = 'yes-btn';
+    yesBtn.innerText = 'Yes'
+    buttonDiv.appendChild(yesBtn);
+    div.appendChild(buttonDiv);
+
+    yesBtn.addEventListener('click', () => {
+      div.parentNode.removeChild(div);
+      userInput.disabled = false;
+      checkBtn.disabled = false;
+      refreshBtn.disabled = false;
+      time = currentTime;
+      timeText.classList.remove('red');
+      timeText.innerText = time;
+      score = 0;
+      scoreText.innerText = score;
+      initGame();
+    })
+
+    const noBtn = document.createElement('button');
+    noBtn.className = 'no-btn';
+    noBtn.innerText = 'No'
+    buttonDiv.appendChild(noBtn);
+
+    noBtn.addEventListener('click', () => {
+      div.parentNode.removeChild(div);
+      displayMessage('Okay! Thanks for Playing!', '');
+    })
+  }
+}
---
+++ js/words.js
@@ -0,0 +1,206 @@
+const countries = [
+   {
+     name: 'Nigeria',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Canada',
+     hint: 'Country in North America'
+   },
+   {
+     name: 'Romania',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'india',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Ecuador',
+     hint: 'Country in South America'
+   },
+   {
+     name: 'Spain',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Argentina',
+     hint: 'Country in South America'
+   },
+   {
+     name: 'Iran',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Iraq',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Ghana',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'France',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Portugal',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'England',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Cameroon',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Togo',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Nepal',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Japan',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Denmark',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Brazil',
+     hint: 'Country in South America'
+   },
+   {
+     name: 'Uganda',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Paraguay',
+     hint: 'Country in South America'
+   },
+   {
+     name: 'Uruguay',
+     hint: 'Country in South America'
+   },
+   {
+     name: 'Wales',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Qatar',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Scotland',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Italy',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Tanzania',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Morocco',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Algeria',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Bangladesh',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Senegal',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Niger',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Sweden',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Chad',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Russia',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Ukraine',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Taiwan',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Thailand',
+     hint: 'Country in Asia'
+   },
+   {
+     name: 'Croatia',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Angola',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Georgia',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Kenya',
+     hint: 'Country in Africa'
+   },
+   {
+     name: 'Austrailia',
+     hint: 'Country in Oceania'
+   },
+   {
+     name: 'Moldova',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Finland',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Iceland',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Netherlands',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Poland',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Germany',
+     hint: 'Country in Europe'
+   },
+   {
+     name: 'Panama',
+     hint: 'Country in North America'
+   },
+   {
+     name: 'Yemen',
+     hint: 'Country in Asia'
+   }
+  ]
